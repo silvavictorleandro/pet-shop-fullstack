7c3192b1cbebb1a0cb51f7c536b79abbbfaebfb8
Victor Silva <silvavictorleandro@outlook.com>
2024-07-01 12:41:45 -0300

    estilização do menu de cadastro de pet

--- frontend/src/components/AddPet/AddPet.tsx
+++ frontend/src/components/AddPet/AddPet.tsx
@@ -1,3 +1,4 @@
+import { MdClose } from "react-icons/md";
 import * as S from "./styles";
 
 interface AddPetProps {
@@ -11,24 +12,27 @@ export const AddPet: React.FC<AddPetProps> = ({ modalPet, toggleModalPet }) => {
       <S.Modal modal={modalPet}>
         <S.ContainerHeader>
           <S.Title>Adicionar Pet</S.Title>
-          <S.ButtonClose onClick={() => toggleModalPet()}>Fechar</S.ButtonClose>
+          <S.ButtonClose onClick={() => toggleModalPet()}>
+            <MdClose />
+          </S.ButtonClose>
         </S.ContainerHeader>
-        <S.LabelPetName>Nome Pet</S.LabelPetName>
-        <S.InputPetName></S.InputPetName>
-        <S.LabelPetType>Espécie do Pet</S.LabelPetType>
-        <S.InputPetType>
+
+        <S.Label>Nome Pet</S.Label>
+        <S.Input></S.Input>
+        <S.Label>Espécie do Pet</S.Label>
+        <S.InputOption>
           <option selected disabled>
             Selecione o seu pet
           </option>
           <option>Gato</option>
           <option>Cachorro</option>
-        </S.InputPetType>
-        <S.LabelPetBreed>Raça</S.LabelPetBreed>
-        <S.InputPetBreed></S.InputPetBreed>
-        <S.LabelPetBirth>Data de Nascimento do Pet</S.LabelPetBirth>
-        <S.InputPetBirth type="date"></S.InputPetBirth>
-        <S.LabelTutorDoc>CPF do Tutor</S.LabelTutorDoc>
-        <S.InputTutorDoc></S.InputTutorDoc>
+        </S.InputOption>
+        <S.Label>Raça</S.Label>
+        <S.Input></S.Input>
+        <S.Label>Data de Nascimento do Pet</S.Label>
+        <S.Input type="date"></S.Input>
+        <S.Label>CPF do Tutor</S.Label>
+        <S.Input></S.Input>
         <S.ButtonAddPet>Cadastrar Pet</S.ButtonAddPet>
       </S.Modal>
     </S.Fade>
--- frontend/src/components/AddPet/styles.ts
+++ frontend/src/components/AddPet/styles.ts
@@ -18,7 +18,7 @@ export const Fade = styled.div<ModalProps>`
 `;
 
 export const Modal = styled.div<ModalProps>`
-  background-color: #fff;
+  background-color: #f8f8f8;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
@@ -28,7 +28,7 @@ export const Modal = styled.div<ModalProps>`
   padding: 2rem;
   top: ${(props) => (props.modal ? "50%" : "0")};
   transform: translate(-50%, -50%);
-  width: 500px;
+  width: 400px;
   z-index: 10;
   opacity: ${(props) => (props.modal ? "1" : "0")};
   pointer-events: ${(props) => (props.modal ? "all" : "none")};
@@ -45,61 +45,42 @@ export const ContainerHeader = styled.div`
 export const Title = styled.h2``;
 
 export const ButtonClose = styled.button`
-  padding: 0.3rem 1rem;
+  background-color: red;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem 0.5rem;
 `;
 
-export const LabelPetName = styled.label`
+export const Label = styled.label`
   margin-top: 0.5rem;
 `;
 
-export const InputPetName = styled.input`
+export const Input = styled.input`
+  border: 1px solid black;
+  border-radius: 5px;
   height: 30px;
-  padding: 0 0.3rem;
-  width: 300px;
+  padding: 0.3rem;
+  width: 100%;
 `;
 
 export const LabelPetType = styled.label`
   margin-top: 0.5rem;
 `;
 
-export const InputPetType = styled.select`
+export const InputOption = styled.select`
   height: 30px;
   padding: 0 0.3rem;
-  width: 300px;
-`;
-
-export const LabelPetBreed = styled.label`
-  margin-top: 0.5rem;
-`;
-
-export const InputPetBreed = styled.input`
-  height: 30px;
-  padding: 0 0.3rem;
-  width: 300px;
-`;
-
-export const LabelPetBirth = styled.label`
-  margin-top: 0.5rem;
-`;
-
-export const InputPetBirth = styled.input`
-  height: 30px;
-  padding: 0 0.3rem;
-  width: 300px;
-`;
-
-export const LabelTutorDoc = styled.label`
-  margin-top: 0.5rem;
-`;
-
-export const InputTutorDoc = styled.input`
-  height: 30px;
-  padding: 0 0.3rem;
-  width: 300px;
+  width: 100%;
 `;
 
 export const ButtonAddPet = styled.button`
-  margin-top: 1rem;
-  padding: 0.4rem;
-  width: 100px;
+  background-color: #22c55e;
+  border: none;
+  border-radius: 5px;
+  font-weight: bold;
+  margin-top: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 120px;
 `;
