a303b382e8f1e84b93ab38fc2f3a3caf127a3dd8
Victor Silva <silvavictorleandro@outlook.com>
2024-07-01 06:34:28 -0300

    correção no posicionamento dos cards

--- frontend/src/components/PetList/PetList.tsx
+++ frontend/src/components/PetList/PetList.tsx
@@ -3,8 +3,17 @@ import { useEffect, useState } from "react";
 import * as S from "./styles";
 import { api } from "../../service/api";
 
+interface Pet {
+  id: number;
+  name: string;
+  type: "Cachorro" | "Gato";
+  tutorName: string;
+  breed: string;
+  dateOfBirth: number;
+}
+
 export const PetList: React.FC = () => {
-  const [pets, setPets] = useState([]);
+  const [pets, setPets] = useState<Pet[]>([]);
 
   useEffect(() => {
     getPets();
@@ -19,10 +28,7 @@ export const PetList: React.FC = () => {
     <S.Main>
       <S.Container>
         {pets.map((pet) => (
-          <PetCard
-            // key={pet.id}
-            pets={pet}
-          />
+          <PetCard key={pet.id} pets={pet} />
         ))}
       </S.Container>
     </S.Main>
--- frontend/src/components/PetList/styles.ts
+++ frontend/src/components/PetList/styles.ts
@@ -5,10 +5,19 @@ export const Main = styled.main`
 `;
 
 export const Container = styled.div`
+  align-content: start;
   display: flex;
   flex-wrap: wrap;
   gap: 2rem;
-  justify-content: center;
+  /* justify-content: center; */
   margin: 3rem auto;
-  max-width: 900px;
+  max-width: 814px;
+
+  @media screen and (max-width: 845px) {
+    max-width: 532px;
+  }
+
+  @media screen and (max-width: 563px) {
+    max-width: 250px;
+  }
 `;
